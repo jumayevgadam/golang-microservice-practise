d2fd6dcda8cf562c63773f4b32fb21efb2394dd7
jumayevgadam <hypergadam@gmail.com>
2025-06-28 20:23:03 +0500

    fix: add stock item handler

--- stocks/internal/migrations/20250615153113_stocks_and_sku.sql
+++ stocks/internal/migrations/20250615153113_stocks_and_sku.sql
@@ -26,7 +26,9 @@ CREATE TABLE IF NOT EXISTS stock_items (
     price BIGINT,
     location TEXT,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT unique_user_sku UNIQUE (user_id, sku_id)
 );
 -- +goose StatementEnd
 
